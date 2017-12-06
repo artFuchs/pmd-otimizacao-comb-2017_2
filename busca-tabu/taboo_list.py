678f79e5a542db4320e8fb184c6b3ee2b010986a
GABRIELMUTTI <gabrielmuttiteixeira@gmail.com>
2017-12-06 18:56:36 -0200

    Adicionando lista tabu.

---
+++ busca-tabu/taboo_list.py
@@ -0,0 +1,19 @@
+from queue import Queue
+
+class TabooList:
+
+    def __init__(self, size):
+        self._size = size
+        self._queue = Queue(size)
+        self._length = 0
+
+    def addMovement(self, movement):
+        if self._queue.isFull():
+            self._queue.pop()
+        self._queue.push(movement)
+            
+    def __len__(self):
+        return len(self._queue)
+
+    def __str__(self):
+        return str(self._queue)
